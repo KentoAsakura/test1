98d6527003a516e5934951590470729791ec718f
KentoAsakura <acchakura1201@gmail.com>
2024-08-04 18:46:23 +0900

    最終調整かな

--- app/main.go
+++ app/main.go
@@ -15,6 +15,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"encoding/csv"
 )
 
 type TemplateRenderer struct {
@@ -40,6 +41,7 @@ func Init() {
 	// データベースのマイグレーション
 	DB.AutoMigrate(&User{})
 	DB.AutoMigrate(&UserInfoMation{})
+	DB.AutoMigrate(&AbsenceUser{})
 }
 
 type UserInfoMation struct {
@@ -67,8 +69,19 @@ type User struct {
 	AllergyInfo string
 	// 同伴者
 	Companion int
+
+	ByBusFlag bool
+
 	// Userが入力しない情報
 	UserInfo UserInfoMation
+}
+
+type AbsenceUser struct{
+	gorm.Model
+
+	UserName string
+
+	Message string
 }
 
 func main() {
@@ -80,6 +93,10 @@ func main() {
 
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(http.StatusOK, "index.html", nil)
+	})
+
+	e.GET("/absence",func(c echo.Context)error{
+		return c.Render(http.StatusOK,"absence.html",nil)
 	})
 
 	// ログインフォームの表示用のルーティング
@@ -98,6 +115,7 @@ func main() {
 		return c.Render(http.StatusOK,"dblogin.html",nil)
 	})
 
+	e.GET("/export-csv", exportCSVHandler)
 
 	e.Static("/css", "../css")
 	e.Static("/js", "../js")
@@ -109,6 +127,8 @@ func main() {
 
 	// ユーザー登録のルーティング
 	e.POST("/signup", signupHandler)
+
+	e.POST("/absenceinfo",absenceHandler)
 
 	e.POST("/db",DbConnectionHandler)
 
@@ -166,6 +186,7 @@ func loginHandler(c echo.Context) error {
 			"PhoneNumber":user.PhoneNumber,
 			"AllergyInfo":user.AllergyInfo,
 			"Companion":user.Companion,
+			"ByBusFlag":user.ByBusFlag,
 		})
 	}
 
@@ -180,31 +201,37 @@ func signupHandler(c echo.Context) error {
 	allergyInfo:=c.FormValue("allergyInfo")
 	companion:=c.FormValue("companion")
 	morw:=c.FormValue("morw")
+	bybus := c.FormValue("bybus")
+
 	var menOrwoman bool
-	if morw!=""{
-		menOrwoman=true
+	if morw != "" {
+		menOrwoman = true
 	}
-	
-	_,err := CreateUser(username, password,allergyInfo,companion,menOrwoman)
-	if err!=nil{
+
+	byBusFlag := false
+	if bybus == "on" {
+		byBusFlag = true
+	}
+
+	_, err := CreateUser(username, password, allergyInfo, companion, menOrwoman, byBusFlag)
+	if err != nil {
 		return c.String(http.StatusInternalServerError, "Failed to create user")
 	}
-	return c.Render(http.StatusOK, "index.html",nil)
+	return c.Render(http.StatusOK, "index.html", nil)
 }
 
-func CreateUser(username, phoneNumber,allergyInfo,companion string,menOrwoman bool)(*User,error){
-	numCompanion,_:=strconv.Atoi(companion)
-	user := &User{UserName: username, PhoneNumber: phoneNumber,AllergyInfo:allergyInfo,Companion:numCompanion,MenOrWomen:menOrwoman}
+func CreateUser(username, phoneNumber, allergyInfo, companion string, menOrwoman, bybus bool) (*User, error) {
+	numCompanion, _ := strconv.Atoi(companion)
+	user := &User{UserName: username, PhoneNumber: phoneNumber, AllergyInfo: allergyInfo, Companion: numCompanion, MenOrWomen: menOrwoman, ByBusFlag: bybus}
 	var userInfo UserInfoMation
 	userInfo.PhoneNumber = user.PhoneNumber
 	userInfo.QRCODE_Number = CreateQRCode(user.PhoneNumber)
 	user.UserInfo = userInfo
 	if err := DB.Create(user).Error; err != nil {
-		return nil,err
+		return nil, err
 	}
-	return user,nil
+	return user, nil
 }
-
 
 func QRScanHandler(c echo.Context) error {
 	// QRコードデータを取得
@@ -244,7 +271,7 @@ func DbConnectionHandler(c echo.Context)error{
 	func DbOpenHandler(c echo.Context) error {
 		var users []User
 		var userInfos []UserInfoMation
-	
+		var absence []AbsenceUser
 		// ユーザーデータを取得
 		if err := DB.Find(&users).Error; err != nil {
 			log.Printf("Failed to get users: %v", err)
@@ -254,9 +281,15 @@ func DbConnectionHandler(c echo.Context)error{
 		if err := DB.Find(&userInfos).Error; err != nil {
 			log.Printf("Failed to get user infos: %v", err)
 		}
+
+		if err:=DB.Find(&absence).Error;err!=nil{
+			log.Printf("Faild to get absenceUser:%v",err)
+		}
+
 		return c.Render(http.StatusOK, "dbhome.html", map[string]interface{}{
 			"Users":      users,
 			"UserInfos":  userInfos,
+			"absence":absence,
 		})
 	}
 
@@ -282,6 +315,7 @@ func InfoUpDateHandler(c echo.Context) error {
     phoneNumber := c.FormValue("phoneNumber")
     allergyInfo := c.FormValue("allergyInfo")
     companion := c.FormValue("companion")
+	bybus:=c.FormValue("bybus")
 
     var menOrWomen bool
     if morw == "on" {
@@ -289,6 +323,11 @@ func InfoUpDateHandler(c echo.Context) error {
     } else {
         menOrWomen = false
     }
+
+	byBusFlag := false
+	if bybus == "on" {
+		byBusFlag = true
+	}
 
     num, err := strconv.Atoi(companion)
     if err != nil {
@@ -306,10 +345,16 @@ func InfoUpDateHandler(c echo.Context) error {
     }
 
     // 更新内容を設定
-    user.UserName = userName
-    user.AllergyInfo = allergyInfo
+	if userName!=""{
+		user.UserName = userName
+	}
+	if allergyInfo!=""{
+		user.AllergyInfo = allergyInfo
+
+	}
     user.Companion = num
     user.MenOrWomen = menOrWomen
+	user.ByBusFlag=byBusFlag
 
     // データベースに保存
     if err := DB.Save(&user).Error; err != nil {
@@ -319,15 +364,15 @@ func InfoUpDateHandler(c echo.Context) error {
     }
 
     return c.JSON(http.StatusOK, map[string]interface{}{
-        "message": userName + "さんの情報が更新されました。",
+        "message": user.UserName + "さんの情報が更新されました。",
     })
 }
 
 
-func ExecuteQueryHandler(c echo.Context)error{
-	query:=c.FormValue("sqlQuery")
+func ExecuteQueryHandler(c echo.Context) error {
+    query := c.FormValue("sqlQuery")
 
-// クエリを実行
+    // クエリを実行
     result := DB.Exec(query)
     if result.Error != nil {
         return c.JSON(http.StatusInternalServerError, map[string]interface{}{
@@ -343,5 +388,72 @@ func ExecuteQueryHandler(c echo.Context)error{
         "message":       "Query executed successfully",
         "rows_affected": rowsAffected,
     })
+}
 
+
+func exportCSVHandler(c echo.Context) error {
+    // ユーザーとその関連情報を取得
+    var users []User
+    DB.Preload("UserInfo").Find(&users) // Preload を使用して関連する UserInfo を事前にロード
+
+    // CSVファイルをメモリ上に作成
+    records := [][]string{
+        {"MenOrWomen", "UserName", "PhoneNumber", "AllergyInfo", "Companion","ByBusFlag", "UserID", "UserPhoneNumber", "QRCODE_Number", "Attend"},
+    }
+
+    for _, user := range users {
+        menOrWomen := "新婦側"
+        if user.MenOrWomen {
+            menOrWomen = "新郎側"
+        }
+        // UserInfo データ
+        userID := fmt.Sprintf("%d", user.UserInfo.UserID)
+        userPhoneNumber := user.UserInfo.PhoneNumber
+        qrcodeNumber := user.UserInfo.QRCODE_Number
+        attend := fmt.Sprintf("%t", user.UserInfo.Attend)
+        byBusFlag := fmt.Sprintf("%t", user.ByBusFlag) // ByBusFlagを文字列に変換
+
+        // レコードの追加
+        records = append(records, []string{
+            menOrWomen,
+            user.UserName,
+            user.PhoneNumber,
+            user.AllergyInfo,
+            fmt.Sprintf("%d", user.Companion),
+			byBusFlag,
+			userID,
+            userPhoneNumber,
+            qrcodeNumber,
+            attend,
+        })
+    }
+
+    // レスポンスヘッダの設定
+    c.Response().Header().Set("Content-Type", "text/csv")
+    c.Response().Header().Set("Content-Disposition", "attachment; filename=\"users.csv\"")
+    w := csv.NewWriter(c.Response().Writer)
+    w.WriteAll(records) // CSVデータの書き込み
+    w.Flush()
+
+    return nil
+}
+
+
+func absenceHandler(c echo.Context)error{
+	username := c.FormValue("username")
+	message:=c.FormValue("message")
+	
+	_,err := CreateAbsenceUser(username,message)
+	if err!=nil{
+		return c.String(http.StatusInternalServerError, "Failed to create user")
+	}
+	return c.Render(http.StatusOK, "index.html",nil)
+}
+
+func CreateAbsenceUser(username,message string)(*AbsenceUser,error){
+	user := &AbsenceUser{UserName: username,Message:message}
+	if err := DB.Create(user).Error; err != nil {
+		return nil,err
+	}
+	return user,nil
 }
